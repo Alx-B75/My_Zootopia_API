7e12f7e290c467aa89b4e989d2bd1f24e4ab4907
Alex Bunting <alexander_bunting@hotmail.com>
2025-03-16 17:28:48 +0100

    First commit to API repo

---
+++ Fetch_animals_API.py
@@ -0,0 +1,107 @@
+import json
+import os
+import requests
+
+def load_data(name):
+    """
+    Load JSON data from file.
+
+    Args:
+        file_path (str): Takes path to JSON file.
+
+    Returns:
+        dict: The loaded JSON data.
+    """
+    url = 'https://api.api-ninjas.com/v1/animals'
+    headers = {'X-Api-Key': 'Kgvl3+TJa+zpvuyxrX8gxA==wWQrcPQE7PN2LI9u'}
+    params = {'name': 'fox'}
+    response = requests.get(url, headers=headers,params=params)
+
+    if response.status_code == 200:
+        animal_data =response.json()
+        return animal_data
+    else:
+        print(f'Error: {response.status_code} - {response.text}')
+    
+
+def load_template():
+    """
+    Load the HTML template from a file.
+
+    Returns:
+        str: The content of the HTML template.
+    """
+    with open('animals_template.html', 'r') as file:
+        page_data = file.read()
+        return page_data
+
+
+def serialize_animal(animal):
+    """
+    Serialize a single animal's data as per instructions to HTML list.
+
+    Args:
+        animal (dict): The animal data.
+
+    Returns:
+        str: The HTML of animal data.
+    """
+    animal_name = animal.get('name', 'Unknown')
+    animal_diet = animal.get('characteristics', {}).get('diet', 'Unknown')
+    animal_location = animal.get('locations', ['Unknown'])[0]
+    animal_type = animal.get('characteristics', {}).get('type', 'Unknown')
+    
+    output = f"""
+    <li class="cards__item">
+        <div class="card__title"><strong>Name: {animal_name}</strong></div>
+        <div class="card__text"><strong>Diet:</strong> {animal_diet}</div>
+        <div class="card__text"><strong>Location:</strong> {animal_location}</div>
+        <div class="card__text"><strong>Type:</strong> {animal_type}</div>
+    </li>"""
+    return output
+
+
+def get_animal_data(animal_data):
+    """
+    Gen HTML for a list of animals.
+
+    Args:
+        animal_data (list): The list of animal data.
+
+    Returns:
+        str: The HTML representation of the animal data.
+    """
+    output = ''
+    for animal in animal_data:
+        output += serialize_animal(animal)
+    return output
+
+
+def replace_data(animals_data, page_template):
+    """
+    Replace the placeholder in the template with the animal data.
+
+    Args:
+        animals_data (list): The list of animal data.
+        page_template (str): The HTML template content.
+
+    Returns:
+        str: The updated HTML with the animal data.
+    """
+    new_data = get_animal_data(animals_data)
+    page_template = page_template.replace('__REPLACE_ANIMALS_INFO__', new_data)
+    return page_template
+
+
+def main():
+    """
+    Main function to gen animals HTML.
+    """
+    animals_data = load_data('animals_data.json')
+    page_template = load_template()
+    updated_page = replace_data(animals_data, page_template)
+    with open('animals.html', 'w') as file:
+        file.write(updated_page)
+
+if __name__ == '__main__':
+    main()
